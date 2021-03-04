a325b7f9dc5564a6174f5b24c8f331aff2159cec
adrihermi <adrihermi@gmai.com>
2021-03-04 17:44:00 +0100

    Cargar alumnos de la bd

--- php/cargarAlumnos.php
+++ php/cargarAlumnos.php
@@ -1,8 +1,7 @@
 <?php 
     require("conexion.php");
 	$consulta = "SELECT * 
-                 FROM alumnos as al JOIN estados_alumnos as ea JOIN estados as es ON (al.dni_alumno = ea.dni_alumno AND ea.id_estado = es.id_estado)
-                 WHERE id_aula = ".$_POST["id_aula"];
+                 FROM alumnos";
 	$saida = array();
 	if ($datos = $conexion->query($consulta)) {   		
 		while ($alumno = $datos->fetch_object()) {
