ac956e84ea3f619e7dd2c11269d0c25dcf3d0ab4
annjolie <annjolie.lourido@gmail.com>
2021-03-13 15:21:22 +0100

    formulario de alumno completado

--- js/usuario.js
+++ js/usuario.js
@@ -2,5 +2,23 @@ $(function() {
     $("#form-alumno").hide();
     $("#alta-alumno").click(function() {
         $("#form-alumno").show();
+        $(".bienvenido").text("Dar de alta un alumno")
     });
+
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation');
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function(form) {
+            form.addEventListener('submit', function(event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+
+                form.classList.add('was-validated');
+            }, false)
+        });
 })
