d22e4eb7b7f052d9eba71a547afe9cce675aed76
David Enrique González Hernández <dev.dvdgzlez@gmail.com>
2021-03-19 15:40:10 +0100

    cargando alumnos por aula

---
+++ php/cargarAlumnosPorAula.php
@@ -0,0 +1,15 @@
+<?php 
+    require("conexion.php");
+	$aula = $_POST['aula'];
+	$consulta = "SELECT * 
+                 FROM alumnos WHERE id_aula='$aula'";
+	$saida = array();
+	if ($datos = $conexion->query($consulta)) {   		
+		while ($alumno = $datos->fetch_object()) {
+			$saida[] = $alumno;
+		}
+		$datos->close();
+	}
+	$conexion->close();
+	echo json_encode($saida);
+?>
