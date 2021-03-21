561c94febe3d5ce3eef84d06b20ab7a13847385f
adrihermi <adrihermi@gmai.com>
2021-03-21 23:28:18 +0100

    Modificaciones cambiarEstadoAlumno

--- php/cambiarEstadoAlumno.php
+++ php/cambiarEstadoAlumno.php
@@ -1,12 +1,20 @@
 <?php 
     require("conexion.php");
 	$consulta = "INSERT INTO estados_alumnos (fecha, id_alumno, id_estado) VALUES (".$_POST['fecha'].", ".$_POST['id_alumno'].", ".$_POST['id_estado'].")";
-	$saida = '';
+	$saida = array();
 	if ($conexion->query($consulta)) {   		
-		$saida = 'ok';
-	}else{
-        $saida = 'erro';
-    }
+		if($_POST['id_estado'] == 3){
+			$consulta = "SELECT posicion_x, posicion_y 
+						 FROM posicion_alumnos 
+						 WHERE id_alumno = ".$_POST['id_alumno']."";
+			if ($datos = $conexion->query($consulta)) {   		
+				while ($alumno = $datos->fetch_object()) {
+					$saida[] = $alumno;
+				}
+				$datos->close();
+			}
+		}
+	}
 	$conexion->close();
-	echo $saida;
+	echo json_encode($saida);
 ?>
