8c2d18d92e490193213ba54abc8c1115e3160acf
adrihermi <adrihermi@gmai.com>
2021-03-21 22:43:55 +0100

    Modificación inserirEstados.php

--- php/inserirEstados.php
+++ php/inserirEstados.php
@@ -1,9 +1,10 @@
 <?php 
     require("conexion.php");
-	$consulta = "INSERT INTO `estados`(`descripcion`) VALUES ('Positivo');"+
-                "INSERT INTO `estados`(`descripcion`) VALUES ('Espera resultados PCR');"+
-                "INSERT INTO `estados`(`descripcion`) VALUES ('En contacto con un positivo');"+
-                "INSERT INTO `estados`(`descripcion`) VALUES ('Normal');";+
+	$consulta = "INSERT INTO estados (descripcion, id_estado) VALUES ('Normal', 1);"+
+                "INSERT INTO estados (descripcion, id_estado) VALUES ('Espera resultados PCR', 2);"+
+                "INSERT INTO estados (descripcion, id_estado) VALUES ('Positivo', 3);"+
+                "INSERT INTO estados (descripcion, id_estado) VALUES ('En contacto con un positivo', 4);";
+                
 	$saida = '';
 	if ($conexion->query($consulta)) {   		
 		$saida = 'ok';
