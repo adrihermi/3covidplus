27b7af8b548426292c019d9e4e2503e39fd89865
adrihermi <adrihermi@gmai.com>
2021-03-21 20:27:11 +0100

    Modificacion posicion_alumnos

--- php/inserirPosicionAlumno.php
+++ php/inserirPosicionAlumno.php
@@ -1,6 +1,6 @@
 <?php 
     require("conexion.php");
-	$consulta = "INSERT INTO estados_alumnos (id_aula, id_alumno, posicion_x, posicion_y) VALUES (".$_POST['id_aula'].", ".$_POST['id_alumno'].", '".$_POST['posicion_x']."', '".$_POST['posicion_y']."')";
+	$consulta = "INSERT INTO posicion_alumnos (id_aula, id_alumno, posicion_x, posicion_y) VALUES (".$_POST['id_aula'].", ".$_POST['id_alumno'].", '".$_POST['posicion_x']."', '".$_POST['posicion_y']."')";
 	$saida = '';
 	if ($conexion->query($consulta)) {   		
 		$saida = 'ok';
