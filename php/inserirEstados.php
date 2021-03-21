b6b358857d1d93d7c931ad81cebea597a0fd62c5
adrihermi <adrihermi@gmai.com>
2021-03-21 23:10:44 +0100

    Correccion

--- php/inserirEstados.php
+++ php/inserirEstados.php
@@ -3,8 +3,7 @@
 	$consulta = "INSERT INTO estados (descripcion, id_estado) VALUES ('Normal', 1);"+
                 "INSERT INTO estados (descripcion, id_estado) VALUES ('Espera resultados PCR', 2);"+
                 "INSERT INTO estados (descripcion, id_estado) VALUES ('Positivo', 3);"+
-                "INSERT INTO estados (descripcion, id_estado) VALUES ('En contacto con un positivo', 4);";
-                
+                "INSERT INTO estados (descripcion, id_estado) VALUES ('En contacto con un positivo', 4);";      
 	$saida = '';
 	if ($conexion->query($consulta)) {   		
 		$saida = 'ok';
