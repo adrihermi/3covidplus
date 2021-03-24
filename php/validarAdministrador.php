b47672cafedd59d6fd1a2b9c6dfac3e60a7f7425
David Enrique González Hernández <dev.dvdgzlez@gmail.com>
2021-03-24 22:13:00 +0100

    validacion de admin

---
+++ php/validarAdministrador.php
@@ -0,0 +1,15 @@
+<?php 
+    require("conexion.php");
+	$consulta = "SELECT permisos FROM usuarios WHERE dni_usuario = '".$_SESSION['usuario']."'";
+	$saida = 'erro';
+	if ($datos = $conexion->query($consulta)) {   		
+		while ($usuario = $datos->fetch_object()) {
+			if ($usuario->permisos == "Administrador") {
+				$saida = "ok";
+			}
+			break;
+		}
+	}
+	$conexion->close();
+	echo $saida;
+?>
