c27a3240a23287003b218cf454c6ca6da8df1e28
adrihermi <adrihermi@gmai.com>
2021-03-24 20:57:12 +0100

    Correcciones

--- php/cargarAlumnoPorId.php
+++ php/cargarAlumnoPorId.php
@@ -1,6 +1,6 @@
 <?php 
     require("conexion.php");
-	$consulta = "SELECT al.nombre, al.apellido1, al.apellido2, al.telefono, al.email, al.email_tutor_legal, al.observaciones, al.dni_alumno, au.nombre as 'nombre_aula', au.capacidad as 'capacidad_aula', al.id_aula
+	$consulta = "SELECT al.nombre, al.apellido1, al.apellido2, al.telefono, al.email, al.email_tutor_legal, al.observaciones, al.dni_alumno, al.fecha_nacimiento, al.genero, au.nombre as 'nombre_aula', au.capacidad as 'capacidad_aula', al.id_aula
                  FROM alumnos as al
 				 INNER JOIN aulas as au on al.id_aula=au.id_aula WHERE al.id_alumno='".$_GET["id"]."'";
 	$saida = "";
