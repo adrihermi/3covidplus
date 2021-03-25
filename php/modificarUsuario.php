ef6c8cc7b75e70bd82a0f76c3a6f72447d3dabe1
adrihermi <adrihermi@gmai.com>
2021-03-25 10:47:23 +0100

    Correcciones

--- php/modificarUsuario.php
+++ php/modificarUsuario.php
@@ -1,6 +1,16 @@
 <?php 
     require("conexion.php");
-	$consulta = "UPDATE usuarios SET nombre='".$_POST['nombre']."', apellido1='".$_POST['apellido1']."', apellido2='".$_POST['apellido2']."', fecha_nacimiento='".$_POST['fecha_nacimiento']."', genero='".$_POST['genero']."', telefono='".$_POST['telefono']."', email='".$_POST['email']."', permisos='".$_POST['permisos']."', dni_usuario='".$_POST['dni_usuario']."', contraseña= '".$_POST['contraseña']."' 
+	$consulta = "UPDATE usuarios SET 
+	nombre='".$_POST['nombre']."', 
+	apellido1='".$_POST['apellido1']."', 
+	apellido2='".$_POST['apellido2']."', 
+	fecha_nacimiento='".$_POST['fecha_nacimiento']."', 
+	genero='".$_POST['genero']."', 
+	telefono='".$_POST['telefono']."', 
+	email='".$_POST['email']."', 
+	permisos='".$_POST['permisos']."', 
+	dni_usuario='".$_POST['dni_usuario']."', 
+	contraseña= '".$_POST['contraseña']."' 
 		WHERE id_usuario=".$_POST['id_usuario'];
 	$saida = '';
 	if ($conexion->query($consulta)) {   		
